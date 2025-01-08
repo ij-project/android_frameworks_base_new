8a2cd717d5c236a7038c304b0c9eb0990d281778
Ij Israfil <israfilkhan494@gmail.com>
2025-01-08 06:52:51 +0000

    Revert "SystemUI: Introduce Volume QS Tile"
    This reverts commit 97f2efd70ef2f648d65489c967f23cc71a176f95.
    
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanelControllerBase.java
@@ -42,7 +42,6 @@ import com.android.systemui.qs.customize.QSCustomizerController;
 import com.android.systemui.qs.external.CustomTile;
 import com.android.systemui.qs.logging.QSLogger;
 import com.android.systemui.qs.tiles.FlashlightStrengthTile;
-import com.android.systemui.qs.tiles.VolumeControlTile;
 import com.android.systemui.qs.tileimpl.QSTileViewImpl;
 import com.android.systemui.qs.tileimpl.SliderQSTileViewImpl;
 import com.android.systemui.qs.tileimpl.TouchableQSTile;
@@ -378,8 +377,7 @@ public abstract class QSPanelControllerBase<T extends QSPanel> extends ViewContr
             longPressEffect = null;
         }
         final QSTileViewImpl tileView;
-        if (FlashlightStrengthTile.TILE_SPEC.equals(tile.getTileSpec())
-            || VolumeControlTile.TILE_SPEC.equals(tile.getTileSpec())) {
+        if (FlashlightStrengthTile.TILE_SPEC.equals(tile.getTileSpec())) {
             TouchableQSTile touchableTile = (TouchableQSTile) tile;
             tileView = new SliderQSTileViewImpl(
                     getContext(),
